5df9257b86825db37942072b1e79c0c0f688c588
raphaelachddou <44444023+raphaelachddou@users.noreply.github.com>
2018-11-16 10:19:52 +0100

    new preprocessing file

---
+++ medical_imaging_preprocessing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 16 10:16:39 2018
+
+@author: raphael
+"""
+
+#medical imaging file to preprocess the data
+
+import numpy as np
+A = np.arange(1,10,1)
+print(A)
